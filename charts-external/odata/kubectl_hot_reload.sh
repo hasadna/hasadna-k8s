182f90d45ae776949993ae9dde6252e2a892d182
Ori Hoch <ori@uumpa.com>
2018-08-14 23:41:04 +0300

    add missing namespace to ckan hot reload script

--- charts-external/odata/kubectl_hot_reload.sh
+++ charts-external/odata/kubectl_hot_reload.sh
@@ -5,7 +5,7 @@ COMMIT_MESSAGE="${1}"
 if echo "${COMMIT_MESSAGE}" | grep 'automatic update of odata-ckan' >/dev/null 2>&1; then
     echo hot reloading odata ckan
     CKAN_POD=`kubectl -n odata get pods -l app=ckan -o 'jsonpath={.items[0].metadata.name}'` &&\
-    kubectl exec $CKAN_POD /entrypoint.sh update-ckanext
+    kubectl -n odata exec $CKAN_POD /entrypoint.sh update-ckanext
     [ "$?" != "0" ] && exit 1
     exit 0
 fi
