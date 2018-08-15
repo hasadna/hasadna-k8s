fd3c12d35cce95b3e5e564cd2a65b7194808cfb1
Ori Hoch <ori@uumpa.com>
2018-08-15 14:10:14 +0300

    support github deploy keys for continuous deployment

--- apps_travis_script.sh
+++ apps_travis_script.sh
@@ -25,51 +25,78 @@ elif [ "${1}" == "deploy" ]; then
     docker tag "${DOCKER_IMAGE}:latest" "${DOCKER_IMAGE}:${tag}" &&\
     docker push "${DOCKER_IMAGE}:${tag}"
     [ "$?" != "0" ] && echo failed docker push && exit 1
+    if [ "${SSH_DEPLOY_KEY_OPENSSL_CMD}" != "" ]; then
+        PUSH_PARAMS="git@github.com:${K8S_OPS_REPO_SLUG}.git ${K8S_OPS_REPO_BRANCH}"
+        ! "${SSH_DEPLOY_KEY_OPENSSL_CMD}" && echo failed to decrypt github deploy key && exit 1
+        SSH_DEPLOY_KEY_VOLUME_ARG='-v "'"`pwd`/${DEPLOY_VALUES_CHART_NAME}_github_deploy_key.id_rsa:/${DEPLOY_VALUES_CHART_NAME}_github_deploy_key.id_rsa"'"'
+        SSH_DEPLOY_KEY_ENV_ARG="-e SSH_DEPLOY_KEY_FILE=/${DEPLOY_VALUES_CHART_NAME}_github_deploy_key.id_rsa"
+    else
+        PUSH_PARAMS="https://${GITHUB_TOKEN}@github.com/${K8S_OPS_REPO_SLUG}.git ${K8S_OPS_REPO_BRANCH}"
+    fi
     docker run -e CLONE_PARAMS="--branch ${K8S_OPS_REPO_BRANCH} https://github.com/${K8S_OPS_REPO_SLUG}.git" \
                -e YAML_UPDATE_JSON='{"'"${DEPLOY_VALUES_CHART_NAME}"'":{"'"${DEPLOY_VALUES_IMAGE_PROP}"'":"'"${DOCKER_IMAGE}:${tag}"'"}}' \
                -e YAML_UPDATE_FILE="${DEPLOY_YAML_UPDATE_FILE}" \
                -e GIT_USER_EMAIL="${DEPLOY_GIT_EMAIL}" \
                -e GIT_USER_NAME="${DEPLOY_GIT_USER}" \
                -e GIT_COMMIT_MESSAGE="${DEPLOY_COMMIT_MESSAGE}" \
-               -e PUSH_PARAMS="https://${GITHUB_TOKEN}@github.com/${K8S_OPS_REPO_SLUG}.git ${K8S_OPS_REPO_BRANCH}" \
+               -e PUSH_PARAMS="${PUSH_PARAMS}" \
+               $SSH_DEPLOY_KEY_VOLUME_ARG $SSH_DEPLOY_KEY_ENV_ARG \
                orihoch/github_yaml_updater
     [ "$?" != "0" ] && echo failed github yaml update && exit 1
 
 elif [ "${1}" == "setup" ]; then
     echo "Setting up continuous deployment for an external app repository"
+    echo Create a dedicated user on docker hub for each project and use organizations to limit access
+    echo The user should have write access to the relevant project
     read -p "Docker user: " DOCKER_USER
-    read -p "Docker image name (not including the user prefix): " DOCKER_IMAGENAME
     read -p "Docker password: " DOCKER_PASS
+    read -p "Docker image (repo/name): " DOCKER_IMAGE
+    echo the K8S repo will usually be hasadna/hasadna-k8s on branch master
     read -p "K8S repo slug: " K8S_OPS_REPO_SLUG
     read -p "K8S repo branch: " K8S_OPS_REPO_BRANCH
+    echo details of the app configuration on the k8s repo
     read -p "App's chart name: " DEPLOY_VALUES_CHART_NAME
     read -p "App's image property name: " DEPLOY_VALUES_IMAGE_PROP
-    read -p "App's repo slug" APP_REPO_SLUG
+    echo the app repo slug is used to get the encrypted travis env vars
+    read -p "App's repo slug: " APP_REPO_SLUG
+    echo this is the file in the k8s repo that stores the updated image names
     read -p "Relative path to values.auto-updated.yaml in the K8S repo: " DEPLOY_YAML_UPDATE_FILE
+    echo these details are only used in the commit message
     read -p "Git user email: " DEPLOY_GIT_EMAIL
     read -p "Git user name: " DEPLOY_GIT_USER
-    read -p "Github token: " GITHUB_TOKEN
+    echo this will be the commit message on updates to the k8s repo
+    read -p "Automatic update commit message: " DEPLOY_COMMIT_MESSAGE
+    echo create a deploy key for the k8s repo with write permissions
+    # read -p "GitHub token: " GITHUB_TOKEN
+    read -p "Path to the private ssh deploy key: " SSH_DEPLOY_KEY
     ENCRYPTED_DOCKER_USER=$(travis encrypt --repo "${APP_REPO_SLUG}" "DOCKER_USER=${DOCKER_USER}" --no-interactive)
     ENCRYPTED_DOCKER_PASS=$(travis encrypt --repo "${APP_REPO_SLUG}" "DOCKER_PASS=${DOCKER_PASS}" --no-interactive)
-    ENCRYPTED_GITHUB_TOKEN=$(travis encrypt --repo "${APP_REPO_SLUG}" "GITHUB_TOKEN=${GITHUB_TOKEN}" --no-interactive)
-    echo "Use the following .travis.yml directly or integrate with existing .travis.yml"
-    echo "--"
+    # ENCRYPTED_GITHUB_TOKEN=$(travis encrypt --repo "${APP_REPO_SLUG}" "GITHUB_TOKEN=${GITHUB_TOKEN}" --no-interactive)
+    SSH_DEPLOY_KEY_OPENSSL_CMD=$(travis encrypt-file --repo "${APP_REPO_SLUG}" \
+                                                     "${SSH_DEPLOY_KEY}" \
+                                                     "${DEPLOY_VALUES_CHART_NAME}_github_deploy_key.id_rsa.enc" \
+                                                     --decrypt-to "${DEPLOY_VALUES_CHART_NAME}_github_deploy_key.id_rsa" \
+                                                     -p --no-interactive | grep '^openssl ')
+    echo 1. copy the key file to the app repo and commit: "${DEPLOY_VALUES_CHART_NAME}_github_deploy_key.id_rsa.enc"
+    echo 2. update the app .travis.yml pasting the following directly or integrate with existing .travis.yml
+    echo
+    echo
     echo "language: bash
 sudo: required
 env:
   global:
     - secure: ${ENCRYPTED_DOCKER_USER}
     - secure: ${ENCRYPTED_DOCKER_PASS}
-    - secure: ${ENCRYPTED_GITHUB_TOKEN}
     - K8S_OPS_REPO_BRANCH=${K8S_OPS_REPO_BRANCH}
     - K8S_OPS_REPO_SLUG=${K8S_OPS_REPO_SLUG}
-    - DOCKER_IMAGE=${DOCKER_USER}/${DOCKER_IMAGENAME}
+    - DOCKER_IMAGE=${DOCKER_IMAGE}
     - DEPLOY_YAML_UPDATE_FILE=${DEPLOY_YAML_UPDATE_FILE}
     - DEPLOY_VALUES_CHART_NAME=${DEPLOY_VALUES_CHART_NAME}
     - DEPLOY_VALUES_IMAGE_PROP=${DEPLOY_VALUES_IMAGE_PROP}
-    - DEPLOY_COMMIT_MESSAGE="'"'"automatic update of ${DOCKER_IMAGENAME}"'"'"
+    - DEPLOY_COMMIT_MESSAGE="'"'"${DEPLOY_COMMIT_MESSAGE}"'"'"
     - DEPLOY_GIT_EMAIL=${DEPLOY_GIT_EMAIL}
     - DEPLOY_GIT_USER=${DEPLOY_GIT_USER}
+    - SSH_DEPLOY_KEY_OPENSSL_CMD="'"'"${SSH_DEPLOY_KEY_OPENSSL_CMD}"'"'"
 services:
   - docker
 script:
