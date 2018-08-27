ceb0c9b5768385633207deae2d48269aaf33ad87
Ori Hoch <ori@uumpa.com>
2018-08-27 11:42:48 +0300

    odata: upgrade ckan-2.8.1

--- charts-external/odata/utils/ckan-search-index.sh
+++ charts-external/odata/utils/ckan-search-index.sh
@@ -5,4 +5,4 @@ PARAMS="$@"
 [ "${PARAMS}" == "" ] && PARAMS="--help"
 
 kubectl exec -it $(kubectl get pods -l app=ckan -o 'jsonpath={.items[0].metadata.name}') \
-    -- bash -c "../../bin/paster search-index -c /etc/ckan/default/development.ini $PARAMS"
+    -- ckan-paster --plugin=ckan search-index -c /etc/ckan/default/development.ini $PARAMS
--- charts-external/odata/utils/ckan-sysadmin.sh
+++ charts-external/odata/utils/ckan-sysadmin.sh
@@ -5,4 +5,4 @@ PARAMS="$@"
 [ "${PARAMS}" == "" ] && PARAMS="--help"
 
 kubectl exec -it $(kubectl get pods -l app=ckan -o 'jsonpath={.items[0].metadata.name}') \
-    -- bash -c "../../bin/paster sysadmin -c /etc/ckan/default/development.ini $PARAMS"
+    -- ckan-paster --plugin=ckan sysadmin -c /etc/ckan/default/development.ini $PARAMS
