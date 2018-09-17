ad333041ac255f954764f9279b4829a782352f1e
Ori Hoch <ori@uumpa.com>
2018-09-17 12:59:34 +0300

    improve helm lint to support chart values from environments

--- helm_lint_all.sh
+++ helm_lint_all.sh
@@ -4,13 +4,22 @@ RES=0
 
 echo "Linting all charts"
 for CHART_NAME in `ls charts-external`; do
-    helm lint charts-external/$CHART_NAME 2>/dev/null | grep 'ERROR' | grep -v 'Chart.yaml: version is required'
-    if [ "$?" == "0" ]; then
-        echo "${CHART_NAME}: failed lint"
-        RES=1
+    LINT_ENVIRONMENTS=$(eval echo `./read_yaml.py charts-config.yaml $CHART_NAME lint-environments 2>/dev/null`)
+    if [ "${LINT_ENVIRONMENTS}" != "" ]; then
+        for LINT_ENVIRONMENT in $LINT_ENVIRONMENTS; do
+            echo LINT_ENVIRONMENT=$LINT_ENVIRONMENT
+            ! ./helm_lint_external_chart.sh $CHART_NAME $LINT_ENVIRONMENT && RES=1
+        done
     else
-        echo "${CHART_NAME}: OK"
+        helm lint charts-external/$CHART_NAME 2>/dev/null | grep 'ERROR' | grep -v 'Chart.yaml: version is required'
+        if [ "$?" == "0" ]; then
+            echo "${CHART_NAME}: failed lint"
+            RES=1
+        else
+            echo "${CHART_NAME}: OK"
+        fi
     fi
+
 done
 
 exit $RES
