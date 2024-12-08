75360b1c08aa17f03e9e03c08c6c784e06b58b5d
ori <ori@uumpa.com>
2024-12-08 20:10:09 +0200

    add argoevents - github

--- apps/hasadna-argocd/manifests/render_templates.sh
+++ apps/hasadna-argocd/manifests/render_templates.sh
@@ -11,4 +11,8 @@ GITHUB_CLIENT_SECRET="$(echo "${DATA}" | jq -r .github_client_secret)" &&\
 cp -f apps/hasadna-argocd/manifests/patch-argocd-cm.yaml.template apps/hasadna-argocd/manifests/patch-argocd-cm.yaml &&\
 sed -i "s/__dex.config.connectors.github.clientID__/${GITHUB_CLIENT_ID}/" apps/hasadna-argocd/manifests/patch-argocd-cm.yaml &&\
 sed -i "s/__dex.config.connectors.github.clientSecret__/${GITHUB_CLIENT_SECRET}/" apps/hasadna-argocd/manifests/patch-argocd-cm.yaml &&\
+DATA="$(vault read kv/data/Projects/iac/outputs/hasadna_argocd -format=json | jq .data.data)" &&\
+GITHUB_WEBHOOK_SECRET="$(echo "${DATA}" | jq -r .github_webhook_secret)" &&\
+cp -f apps/hasadna-argocd/manifests/github-secret.yaml.template apps/hasadna-argocd/manifests/github-secret.yaml &&\
+sed -i "s/__github_webhook_secret__/${GITHUB_WEBHOOK_SECRET}/" apps/hasadna-argocd/manifests/github-secret.yaml &&\
 echo OK
