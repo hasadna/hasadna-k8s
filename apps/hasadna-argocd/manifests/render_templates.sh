01c0976d3100140c24695e0e3818a51564a4863f
Ori Hoch <ori@uumpa.com>
2025-05-24 17:21:22 +0300

    fix argocd oauth for rke2

--- apps/hasadna-argocd/manifests/render_templates.sh
+++ apps/hasadna-argocd/manifests/render_templates.sh
@@ -6,8 +6,8 @@
 [ "${VAULT_TOKEN}" == "" ] && echo missing VAULT_TOKEN env var && exit 1
 
 DATA="$(vault read kv/data/Projects/k8s/argocd -format=json | jq .data.data)" &&\
-GITHUB_CLIENT_ID="$(echo "${DATA}" | jq -r .github_client_id)" &&\
-GITHUB_CLIENT_SECRET="$(echo "${DATA}" | jq -r .github_client_secret)" &&\
+GITHUB_CLIENT_ID="$(echo "${DATA}" | jq -r .rke2_github_client_id)" &&\
+GITHUB_CLIENT_SECRET="$(echo "${DATA}" | jq -r .rke2_github_client_secret)" &&\
 cp -f apps/hasadna-argocd/manifests/patch-argocd-cm.yaml.template apps/hasadna-argocd/manifests/patch-argocd-cm.yaml &&\
 sed -i "s/__dex.config.connectors.github.clientID__/${GITHUB_CLIENT_ID}/" apps/hasadna-argocd/manifests/patch-argocd-cm.yaml &&\
 sed -i "s/__dex.config.connectors.github.clientSecret__/${GITHUB_CLIENT_SECRET}/" apps/hasadna-argocd/manifests/patch-argocd-cm.yaml &&\
