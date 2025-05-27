6242812223117697461872e4121101343a2b16a1
Ori Hoch <ori@uumpa.com>
2025-05-27 21:17:34 +0300

    rke2 upgrade

--- apps/argoevents/manifests/upgrade.sh
+++ apps/argoevents/manifests/upgrade.sh
@@ -15,8 +15,4 @@ echo "# downloaded $(date +%Y-%m-%d) from:" > apps/argoevents/manifests/install.
 echo "#   ${URL}" >> apps/argoevents/manifests/install.yaml
 curl -sL "${URL}" >> apps/argoevents/manifests/install.yaml
 
-# upgrade side container to fix bug
-sed -i 's|natsio/nats-server-config-reloader:0.14.0|natsio/nats-server-config-reloader:0.14.1|' \
-  apps/argoevents/manifests/install.yaml
-
 echo OK
