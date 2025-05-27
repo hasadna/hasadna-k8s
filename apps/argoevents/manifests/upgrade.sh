b8390f6515b8ab99d232dfaec3ca78753585cda1
Ori Hoch <ori@uumpa.com>
2025-05-27 20:43:19 +0300

    rke2 upgrade

--- apps/argoevents/manifests/upgrade.sh
+++ apps/argoevents/manifests/upgrade.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 VERSION="${1}"
 
 if [[ -z "${VERSION}" ]]; then
   echo "Usage: $0 <version>"
   exit 1
-fi &&\
-echo Upgrading ArgoEvents to "${VERSION}" &&\
-URL="https://raw.githubusercontent.com/argoproj/argo-events/${VERSION}/manifests/install.yaml" &&\
-echo "# downloaded $(date +%Y-%m-%d) from:" > apps/argoevents/manifests/install.yaml &&\
-echo "#   ${URL}" >> apps/argoevents/manifests/install.yaml &&\
-curl -sL "${URL}" >> apps/argoevents/manifests/install.yaml &&\
+fi
+
+echo Upgrading ArgoEvents to "${VERSION}"
+URL="https://raw.githubusercontent.com/argoproj/argo-events/${VERSION}/manifests/install.yaml"
+echo "# downloaded $(date +%Y-%m-%d) from:" > apps/argoevents/manifests/install.yaml
+echo "#   ${URL}" >> apps/argoevents/manifests/install.yaml
+curl -sL "${URL}" >> apps/argoevents/manifests/install.yaml
+
+# upgrade side container to fix bug
+sed -i 's|natsio/nats-server-config-reloader:0.14.0|natsio/nats-server-config-reloader:0.14.1|' \
+  apps/argoevents/manifests/install.yaml
+
 echo OK
