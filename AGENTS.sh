56a6eda7b60fecfa4665f9b79cde05e345cc4337
Ori Hoch <ori@uumpa.com>
2025-05-28 13:51:24 +0300

    add e2e test + use go tfe

--- AGENTS.sh
+++ AGENTS.sh
@@ -13,12 +13,12 @@ cd ..
 
 wget https://releases.hashicorp.com/terraform/1.12.1/terraform_1.12.1_linux_amd64.zip
 unzip terraform_1.12.1_linux_amd64.zip -d /usr/local/bin
-rm terraform_1.12.1_linux_amd64.zip
+rm terraform_1.12.1_linux_amd64.zip /usr/local/bin/LICENSE.txt
 chmod +x /usr/local/bin/terraform
 
 wget https://releases.hashicorp.com/vault/1.19.4/vault_1.19.4_linux_amd64.zip
 unzip vault_1.19.4_linux_amd64.zip -d /usr/local/bin
-rm vault_1.19.4_linux_amd64.zip
+rm vault_1.19.4_linux_amd64.zip /usr/local/bin/LICENSE.txt
 chmod +x /usr/local/bin/vault
 
 touch .AGENTS.sh.completed
