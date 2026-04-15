07121bdec5f34d2a1667d631852d16885984b38b
Ori Hoch <ori@uumpa.com>
2026-04-15 20:05:38 +0300

    add backup frequency support for kopia backups

--- hasadna_k8s/ceph/cli.py
+++ hasadna_k8s/ceph/cli.py
@@ -13,9 +13,10 @@ def main():
 @main.command()
 @click.argument('namespace')
 @click.argument('pvc_name')
-def pvc_backup(namespace, pvc_name):
+@click.option('--with-weekly', is_flag=True)
+def pvc_backup(**kwargs):
     from .pvc_backup import main
-    main(namespace, pvc_name)
+    main(**kwargs)
 
 
 @main.command()
@@ -27,11 +28,13 @@ def maintenance(**kwargs):
 
 @main.command()
 @click.option('--with-full-maintenance', is_flag=True)
-def pvc_backup_all(with_full_maintenance):
+@click.option('--with-weekly', is_flag=True)
+@click.option('--with-weekly-on-saturday', is_flag=True)
+def pvc_backup_all(with_full_maintenance, **kwargs):
     from .pvc_backup import main_all
     pvc_backup_failed = False
     try:
-        main_all()
+        main_all(**kwargs)
     except Exception as e:
         if with_full_maintenance:
             traceback.print_exc()
